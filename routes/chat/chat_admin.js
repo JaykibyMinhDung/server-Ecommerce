506ee09c1be43ef7e1e8da8a42211ed533dbdea8
JaykibyMinhDung <109064624+JaykibyMinhDung@users.noreply.github.com>
2023-11-16 23:22:14 +0700

    Create chat_admin.js

---
+++ routes/chat/chat_admin.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+
+const chatroom = require("../../controller/chat/chat");
+
+const firewall = require("../../middleware/auth-admin.js");
+
+router.get("/chatrooms/getById", firewall, chatroom.getRoomChat);
+
+router.get("/chatrooms/getAllRoom", firewall, chatroom.getAllRoomChat);
+
+router.post("/chatrooms/createNewRoom", firewall, chatroom.createRoomChat);
+
+router.put("/chatrooms/addMessage", firewall, chatroom.addNewMessage);
+
+module.exports = router;
