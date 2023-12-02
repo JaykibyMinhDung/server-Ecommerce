cf5d234e30f13cff9b565bb7e55c6f24c437ea8f
JaykibyMinhDung <doanminhdung1729@gmail.com>
2023-12-02 16:25:09 +0700

    updated route logout

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -9,7 +9,7 @@ router.post("/users", auth.login);
 
 router.get("/users/all", firewall, auth.getAllUser);
 
-router.get("/logout", firewall, auth.logout);
+router.get("/admin/logout", firewall, auth.logout);
 
 // router.put("/chatrooms/addMessage", firewall, chatroom.addNewMessage);
 
